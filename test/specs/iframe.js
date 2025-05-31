30d3a0be2380cd6172d1452836748e1df2d39e70
Reymon Mendez <rmendezm@dgii.gov.do>
2025-05-31 19:58:30 -0400

    Agregando test de iFrames parte 2

---
+++ test/specs/iframe.js
@@ -0,0 +1,24 @@
+import { browser, expect } from '@wdio/globals';
+
+describe('IFrame', () => {
+    it('Working with iFrame', async ()=>{
+    //Open URL
+    await browser.url('https://practice.sdetunicorns.com/iframe-sample/');
+
+
+    //access iFrame
+    const iframe = await $('#advanced_iframe');
+    await browser.switchFrame(iframe);
+
+
+    //Verify logo exist in the iFrame
+    await expect($('.hfe-site-logo')).toExist();
+    
+    //Switch to parent frame
+    await browser.switchToParentFrame();
+
+    //Verify parent website
+    await expect($('h1.zak-page-title')).toHaveText('IFrame Sample');
+
+    });
+});
