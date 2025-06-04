bd6c2c710b2994e25c9a7ff6caaf1e8a9e253e1b
Reymon <rjmm2112@gmail.com>
2025-06-03 22:19:19 -0400

    Agregar faker y dependencia

--- wdio.conf.js
+++ wdio.conf.js
@@ -207,8 +207,9 @@ export const config = {
     /**
      * Function to be executed before a test (in Mocha/Jasmine) starts.
      */
-    // beforeTest: function (test, context) {
-    // },
+     beforeTest: async function () {
+        browser.setWindowSize(1000,1000);
+     },
     /**
      * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
      * beforeEach in Mocha)
